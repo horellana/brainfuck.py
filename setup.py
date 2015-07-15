47fab160472e994f194b9410a5f716dc2f130476
Hector Orellana <pulga.p.29@gmail.com>
2015-07-15 00:10:06 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='brainfuck.py',
+      version='0.1.0',
+      description='A brainfuck interpreter written in python',
+      url='https://github.com/juiko/brainfuck.py',
+      license='GPL3',
+      author='Hector Orellana',
+      author_email='hofm92@gmail.com',
+      py_modules=['brainfuck'])
